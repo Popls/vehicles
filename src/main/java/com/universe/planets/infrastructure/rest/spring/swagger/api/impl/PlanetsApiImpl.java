eb3e164e3d2d581270c20bd5634c38dd0ea7a993
mroth-stratio <mroth@stratio.com>
2020-04-19 18:56:56 +0200

    Architecture change. Ref: gitignore have been modified to exclude dtos and specs. Ref: pom have been modified to generate correctly specs. Ref: PlanetsApiImpl have been modified with correct imports.

--- src/main/java/com/universe/planets/infrastructure/rest/spring/swagger/api/impl/PlanetsApiImpl.java
+++ src/main/java/com/universe/planets/infrastructure/rest/spring/swagger/api/impl/PlanetsApiImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.universe.planets.application.service.PlanetService;
 import com.universe.planets.domain.IPlanetDomain;
 import com.universe.planets.infrastructure.rest.spring.mapper.DtoMapper;
+import com.universe.planets.infrastructure.rest.spring.swagger.api.spec.PlanetsApi;
+import com.universe.planets.infrastructure.rest.spring.swagger.dto.PlanetsSummariesDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
